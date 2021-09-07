a6a617c4bccb5d957ae7b91d6436c36980c19118
VeetiVee <veeti.vainikka@gmail.com>
2021-09-07 19:04:55 +0300

    first commit

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,53 @@
+package com.example.bookstore.domain;
+
+public class Book {
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	private String author;
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	private String year;
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	private String isbn;
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	private String price;
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
